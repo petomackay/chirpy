f0218f6595a964290a4783e7b6c55b8c310dfbc5
Peter Mackay <petomackay@gmail.com>
2024-02-03 23:17:49 +0100

    Add a health check

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzCallback)
 
 	corsMux := middlewareCors(mux)
 	server := &http.Server {
@@ -19,6 +20,13 @@ func main() {
 	
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzCallback(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+	return
 }
 
 func middlewareCors(next http.Handler) http.Handler {
