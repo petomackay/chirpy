79adaa2aa9310b8270300f57115e03223621d590
Peter Mackay <petomackay@gmail.com>
2024-02-07 22:23:04 +0100

    Initial validate_chirp endpoint

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -29,6 +31,7 @@ func main() {
 	api_router := chi.NewRouter()
 	api_router.Get("/healthz", healthzCallback)
 	api_router.Get("/reset", ac.resetCallback)
+	api_router.Post("/validate_chirp", validateChirpHandler)
 	r.Mount("/api", api_router)
 
 	admin_router := chi.NewRouter()
@@ -47,6 +50,63 @@ func main() {
 	
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+// TODO: I'll write it out for sake of repetition when learning, but it needs a refactor
+// probably generic handleEror and sendJson functions will do nicely
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "applicaton/json")
+	type chirpParams struct {
+		Body string `json:"body"`
+	}
+
+	type errorBody struct {
+		Error string `json:"error"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	chirp := chirpParams{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		error := errorBody {
+			Error: "Something went wrong",
+		}
+		dat, err := json.Marshal(error)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+                 error := errorBody {
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(error)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+
+	type resp struct {
+		Valid bool `json:"valid"`
+	}
+
+	dat, err := json.Marshal(resp{
+		Valid: true,
+	})
+
+	w.Write(dat)
+	w.WriteHeader(http.StatusOK)
+
 }
 
 func healthzCallback(w http.ResponseWriter, r *http.Request) {
